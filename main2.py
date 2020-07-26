4d4d1e13cd0fae2a9c52a3349f723942d8a0201b
kornevenema <kornevenema@hotmail.com>
2020-07-26 21:03:20 +0200

    creating starting word

--- main2.py
+++ main2.py
@@ -1,8 +1,22 @@
+def get_all_words():
+    with open("filteredWords.txt", "r") as f:
+        return [row.strip() for row in f.readlines() if row.strip() != ""]
+
+
+def get_points(points, word):
+    total = 0
+    for ch in word:
+        total += points[ch]
+    return total
+
+
 class BotPlayer:
     def __init__(self):
         self.all_words = get_all_words()
-        self.poss_words = []
         self.cur_letters = []
+        self.cur_letters_left = []
+        self.poss_words = []
+        self.starting_words = []
         self.points = {
             "a": 1,
             "b": 3,
@@ -32,10 +46,22 @@ class BotPlayer:
             "z": 5
         }
 
+    def set_poss_words(self):
+        for word in self.all_words:
+            good = True
+            temp = list(self.cur_letters)
+            for ch in word:
+                if ch in temp:
+                    temp.remove(ch)
+                else:
+                    good = False
+                    pass
+            if good:
+                self.poss_words.append(word)
+
     def set_cur_letters(self, new_letters):
-        self.cur_letters = new_letters
-        self.get_poss_words()
-        print("current letters and possible words set")
+        self.cur_letters = new_letters.split()
+        self.set_poss_words()
 
     def get_start_word(self):
         max_eff = 0
@@ -43,46 +69,20 @@ class BotPlayer:
         for word in self.poss_words:
             score = get_points(self.points, word)
             eff = score / len(word)
-            if eff > max_eff and len(word) > 3:
+            if eff > max_eff and len(word) > 3 and score > 10:
                 max_eff = eff
-                max_word = [word]
-            elif eff == max_eff:
-                max_word.append(word)
+                max_word = [[word, eff, score]]
+            elif eff == max_eff and score > 10:
+                max_word.append([word, eff, score])
+        self.starting_words = max_word
         return max_word
-
-    def get_poss_words(self):
-        for word in self.all_words:
-            if check_word(self.cur_letters, word):
-                self.poss_words.append(word)
-
-
-def get_all_words():
-    with open("filteredWords.txt", "r") as f:
-        return [row.strip() for row in f.readlines() if row.strip() != ""]
-
-
-def check_word(letters, word):
-    possible = True
-    for ch in word:
-        if ch not in letters:
-            possible = False
-        else:
-            letters.remove(ch)
-    return possible
-
-
-def get_points(points, word):
-    total = 0
-    for ch in word:
-        total += points[ch]
-    return total
 
 
 def main():
     bot = BotPlayer()
-    bot.set_cur_letters(['o', 'r', 'g', 'e', 'd', 'i', 'n', 'n', 'g', 'o', 'h', 'f', 'l'])
-    print(bot.cur_letters)
-    print(bot.get_start_word())
+    bot.set_cur_letters('o p y e d i n n e o h f l')
+    for item in bot.get_start_word():
+        print(item)
 
 
 if __name__ == "__main__":
